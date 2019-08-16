b30d07c64185e4958d4f653cdf1b473d242eb64c
joel <635328166@qq.com>
2019-08-16 15:19:51 +0800

    Site updated: 2019-08-16 15:19:29

--- js/schemes/pisces.js
+++ js/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Fri Aug 16 2019 15:11:40 GMT+0800 (GMT+08:00)
+// build time:Fri Aug 16 2019 15:19:20 GMT+0800 (GMT+08:00)
 $(document).on("DOMContentLoaded",function(){var t=$(".sidebar-inner");var e=CONFIG.sidebar.offset||12;function o(){return $(".header-inner").height()+e}function i(){var t=$("#footer");var e=$(".footer-inner");var o=t.outerHeight()-e.outerHeight();var i=t.outerHeight()+o;return i}function n(){var n=o();var a=i();t.affix({offset:{top:n-e,bottom:a}});$("#sidebar").css({"margin-top":n,"margin-left":"auto"})}function a(){$(window).off(".affix");t.removeData("bs.affix").removeClass("affix affix-top affix-bottom");n()}function r(){var t=window.matchMedia("(min-width: 992px)");t.addListener(function(t){if(t.matches){a()}})}n();r()});
 //rebuild by neat 

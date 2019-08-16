f2aefdbca559c23971f1469b705efec1eed3b6d8
joel <635328166@qq.com>
2019-08-16 15:07:44 +0800

    Site updated: 2019-08-16 15:07:36

--- js/src/dytitle.js
+++ js/src/dytitle.js
@@ -1,3 +1,3 @@
-// build time:Fri Aug 16 2019 15:04:23 GMT+0800 (GMT+08:00)
+// build time:Fri Aug 16 2019 15:07:26 GMT+0800 (GMT+08:00)
 var OriginTitile=document.title;var titleTime;document.addEventListener("visibilitychange",function(){if(document.hidden){$('[rel="shortcut icon"]').attr("href","/TEP.png");document.title="w(ﾟДﾟ)w 出BUG啦！！！！";clearTimeout(titleTime)}else{$('[rel="shortcut icon"]').attr("href","/favicon.png");document.title="♪(^∇^*)又好了。。。 "+OriginTitile;titleTime=setTimeout(function(){document.title=OriginTitile},2e3)}});
 //rebuild by neat 

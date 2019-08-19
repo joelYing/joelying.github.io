2f9630d83651756f46ed9508593ea623967b9625
joel <635328166@qq.com>
2019-08-19 17:56:53 +0800

    Site updated: 2019-08-19 17:56:42

--- js/src/aiguotext.js
+++ js/src/aiguotext.js
@@ -2,7 +2,7 @@ var a_idx = 0;
 jQuery(document).ready(function($) {
     $("body").click(function(e) {
         var a = new Array
-        ("瀵屽己", "姘戜富", "鏂囨槑", "鍜岃皭", "鑷敱", "骞崇瓑", "鍏", "娉曟不", "鐖卞浗", "鏁笟", "璇氫俊", "鍙嬪杽");
+        ("富强", "民主", "文明", "和谐", "自由", "平等", "公正" ,"法治", "爱国", "敬业", "诚信", "友善");
         var $i = $("<span/>").text(a[a_idx]);
         a_idx = (a_idx + 1) % a.length;
         var x = e.pageX,

72cbf93568cfb29c73a06817933ed255c384366a
joel <635328166@qq.com>
2019-09-04 13:36:05 +0800

    Site updated: 2019-09-04 13:35:54

--- dist/aplayermusic.js
+++ dist/aplayermusic.js
@@ -4,7 +4,7 @@ const ap = new APlayer({
     fixed: true, //吸底模式，与迷你模式冲突
     autoplay: false,//默认是否自动播放
     theme: '#FADFA3',
-    loop: 'all',
+    loop: 'one',
     order: 'random',
     preload: 'auto',
     volume: 0.7,//默认音量
@@ -20,6 +20,70 @@ const ap = new APlayer({
             url: 'http://music.163.com/song/media/outer/url?id=22842399.mp3',
             cover: 'http://p1.music.126.net/2996FlCtcEluFUaFG9Y16A==/857619069689686.jpg',
             lrc: '/dist/music/22842399.lrc',
+            theme: '#46718b'
+        },
+        {
+            name: '轻轻的放下',
+            artist: '小柯',
+            url: 'http://www.ytmp3.cn/down/55422.mp3',
+            cover: 'http://p2.music.126.net/b5UY8eG2CwxsU568JH6KdQ==/3244658815024143.jpg',
+            lrc: '/dist/music/29771420.lrc',
+            theme: '#46718b'
+        },
+        {
+            name: '헤어지지 못하는 여자, 떠나가지 못하는 남자',
+            artist: 'Leessang / 郑仁',
+            url: 'http://music.163.com/song/media/outer/url?id=22842426.mp3',
+            cover: 'http://p2.music.126.net/dl6j6G3KvH5477D-7P6KVw==/6641050231879939.jpg',
+            lrc: '/dist/music/22842426.lrc',
+            theme: '#46718b'
+        },
+        {
+            name: '나란 놈은 답은 너다 Prologue',
+            artist: 'Leessang',
+            url: 'http://music.163.com/song/media/outer/url?id=22842409.mp3',
+            cover: 'https://p2.music.126.net/LZ7l36qFBK2nBmPVvUA8Fw==/590437744139070.jpg',
+            // lrc: '/dist/music/22842426.lrc',
+            theme: '#46718b'
+        },
+        {
+            name: '리쌍 부르쓰',
+            artist: 'Leessang',
+            url: 'http://music.163.com/song/media/outer/url?id=1360898625.mp3',
+            cover: 'https://p2.music.126.net/LZ7l36qFBK2nBmPVvUA8Fw==/590437744139070.jpg',
+            // lrc: '/dist/music/22842426.lrc',
+            theme: '#46718b'
+        },
+        {
+            name: 'Yunomi--インドア系ならトラックメイカー (Fdby Remix)',
+            artist: 'Yunomi / nicamoq / Fdby',
+            url: 'http://music.163.com/song/media/outer/url?id=425684779.mp3',
+            cover: 'https://p2.music.126.net/Ez-_OKKKzKsSXzDY15hwTw==/3394192418636766.jpg',
+            lrc: '/dist/music/425684779.lrc',
+            theme: '#46718b'
+        },
+        {
+            name: '삐삐',
+            artist: 'IU',
+            url: 'http://music.163.com/song/media/outer/url?id=1316563427.mp3',
+            cover: 'https://p1.music.126.net/gxo54B2ypqq0Y6tmahBnIw==/109951163596240238.jpg',
+            lrc: '/dist/music/1316563427.lrc',
+            theme: '#46718b'
+        },
+        {
+            name: 'ヤキモチ',
+            artist: '高橋優',
+            url: 'http://music.163.com/song/media/outer/url?id=28885472.mp3',
+            cover: 'https://p1.music.126.net/oPj76RwZ9ChuFoRIhx65xw==/8947825626848871.jpg',
+            lrc: '/dist/music/28885472.lrc',
+            theme: '#46718b'
+        },
+        {
+            name: 'Por Una Cabeza',
+            artist: 'Thomas Newman',
+            url: 'http://music.163.com/song/media/outer/url?id=2007819.mp3',
+            cover: 'https://p1.music.126.net/Qbd2-webBTcERPSSQVB4SQ==/779553754365834.jpg',
+            // lrc: '/dist/music/22842426.lrc',
             theme: '#46718b'
         }
     ]
